e83024d114466ed5f9180131718b78d6281cab20
Samuel Costa <samu.rcosta@gmail.com>
2020-07-21 00:35:55 -0300

    Update to rebuild

--- src/middlewares/database.js
+++ src/middlewares/database.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 async function database(req, res, next) {
+  console.log(process.env.MONGO_URL);
   await mongoose.connect(process.env.MONGO_URL, {
     useCreateIndex: true,
     useNewUrlParser: true,
